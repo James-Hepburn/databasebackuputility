be5daa993c597dd6b2268c7b6e93822dbf9d0ab7
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-09-07 18:56:27 -0600

    Initial commit

---
+++ src/main/java/com/example/databasebackuputility/DatabasebackuputilityApplication.java
@@ -0,0 +1,13 @@
+package com.example.databasebackuputility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasebackuputilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabasebackuputilityApplication.class, args);
+	}
+
+}
